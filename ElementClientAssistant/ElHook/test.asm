61b58da0fa014282fdb8ea189f4947c25e37d6c0
clay.wang <lovejunlic@hotmail.com>
2018-03-02 16:07:30 +0800

    checkin

--- ElementClientAssistant/ElHook/test.asm
+++ ElementClientAssistant/ElHook/test.asm
@@ -1,8 +1,21 @@
+
+EXTERN g_val1:DQ
+EXTERN g_val2:DQ
+
 .CODE
 
 Int_3 PROC
-       int 3;
-       ret;
+		;mov rax,g_val1
+		;mov rbx,g_val2
+		mov rax,rcx
+		mov rbx,rdx
+		add rax,rbx 
+		ret
 Int_3 ENDP
+
+Int_4 PROC
+	mov rax,23	;·µ»Ø123
+	ret
+Int_4 ENDP
 
 END
